c88afc2ed022e6010875b10b5a4d336e2fd57a20
Esam Ahmed <samahmdlyhsan@gmail.com>
2026-05-21 21:04:38 +0300

    modifed files

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/foundation.dart'
+    show kIsWeb; // أضفنا هذا السطر للتحقق من الويب
 import 'package:provider/provider.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
@@ -9,16 +11,21 @@ import 'screens/login_screen.dart';
 import 'screens/register_screen.dart';
 
 void main() async {
+  // التأكد من تهيئة روابط فلاتر
   WidgetsFlutterBinding.ensureInitialized();
 
+  // تهيئة الفايربيز
   await Firebase.initializeApp(
     options: DefaultFirebaseOptions.currentPlatform,
   );
 
-  FirebaseFirestore.instance.settings = const Settings(
-    persistenceEnabled: true,
-    cacheSizeBytes: 100 * 1024 * 1024, // 100 MB
-  );
+  // إعدادات الـ Firestore مع شرط عدم التشغيل على الويب
+  if (!kIsWeb) {
+    FirebaseFirestore.instance.settings = const Settings(
+      persistenceEnabled: true,
+      cacheSizeBytes: 100 * 1024 * 1024, // 100 MB
+    );
+  }
 
   runApp(
     ChangeNotifierProvider(
